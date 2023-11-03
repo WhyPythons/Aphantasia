8d364995fed248a1b939a681540fecb712d53f28
WhyPythons <149730182+WhyPythons@users.noreply.github.com>
2023-11-02 22:43:20 -0400

    Update Game.py
    Wow! CODE! just a few hours ago this was nothing but a blank piece of digital paper, now its a less blank piece of digital paper.
    Woohoo!

--- Game.py
+++ Game.py
@@ -1,1 +1,31 @@
 import random
+def GenerateLevel(level, exp, expthresh):
+  level = 1
+  exp = 0
+  expthresh = 20
+  return level, exp, expthresh
+def GenerateStats(health, strength, intelligence, constitution, agility):
+  health = random.randint(10,20) #Unsure if this should be fixed or random.
+  strength = random.randint(1,10)
+  intelligence = random.randint(1,10)
+  constitution = random.randint(1,10)
+  agility = random.randint(1,10)
+  return health, strength, intelligence, constitution, agility
+def GeneratePlayer():
+    stats = GenerateStats(0,0,0,0,0)
+    level = GenerateLevel(0,0,0)
+    player = {
+    "health": stats[0], 
+    "strength": stats[1], 
+    "intelligence": stats[2], 
+    "constitution": stats[3], 
+    "agility": stats[4],
+    "level": level[0],
+    "exp": level[1],
+    "expthresh": level[2]}
+    return player
+Player = GeneratePlayer()
+print(Player)
+def CharSheet():
+    print(f"health: {Player['level']}")
+#To be continued
