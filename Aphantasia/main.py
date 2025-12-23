ca86ef340ca17ede3d25ee1bbdb600e4af856e08
WhyPythons? <149730182+WhyPythons@users.noreply.github.com>
2025-12-23 00:02:07 -0500

    Framework update (V2)
    Removed some obsolete json, changed some things up in identity, and added some new json.

---
+++ Aphantasia/main.py
@@ -0,0 +1,7 @@
+import items, units, world, render
+
+                                                                                                                                                                                    
+def main():
+    render.main_menu()
+    
+main()
---
+++ Aphantasia/world.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+from enum import Enum
+
+class WorldLevel(Enum):
+    DESOLATE = "desolate"
+    OPTIMISTIC = "optimistic"
+    PROSPEROUS = "prosperous"
+    THRIVING = "thriving"
+    Utopia = "utopia"
+
+@dataclass
+class World:
+    level: WorldLevel
+    coins: int
+    units: dict
+    inventory: dict
+    buildings: dict
+
+@dataclass
+class Building:
+    level: int 
+    name: str
+    description: str
+    occupants: int
+    maxoccupants: int
+    
